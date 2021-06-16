2ea6cbded1dc58ad96aa6ef7b61c4da964b5ff24
Timur <puzino000@gmail.com>
2021-06-16 20:40:10 +0300

    1

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-value = 13
-if not value % 2:
-    print(value, '= 2')
-elif not value % 3:
-    print(value, '= 3')
-else:
-    print('Делится на 2 и на 3')
+# value = 13
+# if not value % 2 and not value % 3:
+#     print('Делится на 2 и на 3')
+# elif not value % 2:
+#     print('Делится на 2')
+# elif not value % 3:
+#     print('Делится на 3')
 
 # value = 'abc'
 # new_value = bool(value)
 # print(new_value)
 
 
-ё12аывывавыаыв3123
+# Тернарный оператор
+my_int = 100
+my_int = my_int // 2 if my_int > 10 else -1
+
+print(my_int)
