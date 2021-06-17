5f0108a7a629a4ed24f9aa10e2d985d920841c40
Timur <puzino000@gmail.com>
2021-06-17 11:44:22 +0300

    1

--- Homeworks - Lesson 3.py
+++ Homeworks - Lesson 3.py
@@ -1,35 +1,35 @@
 # 1) У вас есть переменная value, тип - int. Написать тернарный оператор для переменной new_value по такому
 # правилу: если value меньше 100, то new_value равно половине значения value, в противном случае - противоположне value число
-# value = 100
-# new_value = value // 2 if value < 100 else -1
-# print(new_value)
+value = 100
+new_value = value // 2 if value < 100 else -1
+print(new_value)
 
 # 2) У вас есть переменная value, тип - int. Написать тернарный оператор для переменной new_value по такому
 # правилу: если value меньше 100, то new_value равно 1, в противном случае - 0
-# value = 10
-# new_value = 1 if value < 100 else 0
-# print(new_value)
+value = 10
+new_value = 1 if value < 100 else 0
+print(new_value)
 
 # 3) У вас есть переменная value, тип - int. Написать тернарный оператор для переменной new_value по такому
 # правилу: если value меньше 100, то new_value равно True, в противном случае - False
-# value = 100
-# new_value = True if value < 100 else False
-# print(new_value)
+value = 100
+new_value = True if value < 100 else False
+print(new_value)
 # 4) У вас есть переменная my_str, тип - str. Заменить в my_str все маленькие буквы на большие.
-# my_str = 'qwerty'
-# my_str = my_str.upper()
-# print(my_str)
+my_str = 'qwerty'
+my_str = my_str.upper()
+print(my_str)
 # 5) У вас есть переменная my_str, тип - str. Заменить в my_str все большие буквы на маленькие.
-# my_str = 'QWERTY'
-# my_str = my_str.lower()
-# print(my_str)
+my_str = 'QWERTY'
+my_str = my_str.lower()
+print(my_str)
 # 6) У вас есть переменная my_str, тип - str. Если ее длинна меньше 5, то допишите в конец строки себя же.
 # Пример: было - "qwer", стало - "qwerqwer". Если длинна не меньше 5, то оставить строку как есть.
-# my_str = 'qwer'
-# s = len(my_str)
-# n = 5
-# new_str = my_str * 2 if s < n else my_str
-# print(new_str)
+my_str = 'qwer'
+s = len(my_str)
+n = 5
+new_str = my_str * 2 if s < n else my_str
+print(new_str)
 # 7) У вас есть переменная my_str, тип - str. Если ее длинна меньше 5, то допишите в конец строки перевернутую себя же.
 # Пример: было - "qwer", стало - "qwerrewq". Если длинна не меньше 5, то оставить строку как есть.
 my_str = 'qwer'
